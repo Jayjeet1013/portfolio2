96ac81d74efb692d467eb936b2382e193445e7dd
jayjeet kumar <jayjeet1013@gmail.com>
2022-06-30 03:15:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".typing", {
+    strings:["" ,"web developer", "Nft designer" , "Web3 learner" , "Blogger" , "Youtuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+  
+  
+   
+
+} )
